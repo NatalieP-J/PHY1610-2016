ea0e5d50b574a2b80e5c132434c6b44ba638f12f
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-02-20 17:40:17 -0500

    Created class object to hold energy related constants and functions and modified Makefile to create the appropriate library file

---
+++ hw4/energy.cpp
@@ -0,0 +1,54 @@
+#include <cmath>
+
+class energy{
+
+private:
+
+  // Constants of energy equations
+  const float a = 1; // Joules - energy scale
+  const float b = 0.1; // metres - length scale
+  const float c = 100; // Newtons/metre - spring constant
+  const float d = 0.5; // metres - max spring extension
+  const float f = 2500; // stiffness at max extension
+  const float g = 9.8; // metres/second^2 - gravitational acceleration
+  double mass;
+
+public:
+
+  // Constructor - used to assign mass
+  energy(double m);
+  
+  // Calculate spring potential at a position x
+  double spring_potential(double x){
+    // Return energy in Joules
+    return a*((b/x) + ((pow(d,2))/(f*pow((x-d),2)))-exp((-c*pow((x-b),2))/(2*a)));
+  }
+
+  // Calculate gravitational potential at a position x
+  double grav_potential(double x){
+    // Return energy in Joules
+    return -mass*g*x;
+  }
+
+  // Calculate the total energy
+  double total_energy(double x){
+    // Return energy in Joules
+    return spring_potential(x) + grav_potential(x);
+  }
+  
+  // Tests:
+  // grav_potential == 0 if mass == 0
+  // total_energy==spring_potential if mass == 0
+  // Other notes:
+  // How to handle x==0 case?
+
+  // Destructor
+  ~energy();
+};
+
+energy::energy(double m){
+  mass = m;
+}
+
+energy::~energy(){
+}
