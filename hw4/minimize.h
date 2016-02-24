59013c2360f23545cc0800f01b89259ac53f0384
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-02-24 15:44:11 -0500

    Weird error where I am unable to add a header file to my repository

---
+++ hw4/minimize.h
@@ -0,0 +1,13 @@
+// Natalie Price-Jones Feb 2016
+
+#include "energy.h"
+#include <tuple>
+
+#ifndef MINIMIZE_H
+#define MINIMIZE_H
+
+std::tuple<double,double> f_min_all(double mass,energy mass_energy);
+
+double maximum_load();
+
+#endif
