171241b9c08d18fd3a843b545752779ea1460074
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-04-07 17:25:47 -0400

    Added comments to wave1d.cc

--- hw10/wave1d.cc
+++ hw10/wave1d.cc
@@ -58,16 +58,20 @@ int main(int argc, char* argv[])
     std::cout << "#nper     " << nper    << std::endl;
     std::cout << "#graphics " << int(graphics) << std::endl;
 
+    // Initialize MPI and associated variables. Retrieve rank and size
     int rank, size;
     int ierr;
     ierr = MPI_Init(&argc, &argv);
     ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     ierr = MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-    int local_npnts = (npnts/size); // calculate the number of points (plus guard cells)
-
+    // Calculate the number of points assigned to each process
+    int local_npnts = (npnts/size); // (without guard cells)
+    
+    // Find the number of points left to be assigned after integer division
     int left_over_npnts = npnts % size;
 
+    // Assign each left over point to the processes in numerical sequence  until none are left
     if (left_over_npnts != 0){
       for(int assigned_pts = 0; assigned_pts < left_over_npnts; assigned_pts++){
 	if (rank == assigned_pts){
@@ -76,44 +80,62 @@ int main(int argc, char* argv[])
       }  
     }
 
+    // Find local x bounds for each process. Also track the global index by finding the global
+    // (monolithic) index at which each local array should start
     double local_x1, local_x2;
+    // Find the location of the processes to the left and right
     int left = rank-1;
     int right = rank+1;
-    int tag = 1;
+    // Create tags for the two simultaneous sending processes
+    int tag_x = 0;
+    int tag_i = 1;
+    // Initialize variables for the global index starting point for each process, and the index
+    // information to be sent
     int istart,isend;
     istart = 0;
     isend = 0;
+    
+    // Special left-most edge case
     if (rank==0){
+      // Calculate local bounds on x
       local_x1 = x1;
       local_x2 = x1+(local_npnts*dx);
+      // Find starting global index of rightward process
       isend += local_npnts;
       if (size > 1){
-	ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag,MPI_COMM_WORLD);
-	ierr = MPI_Send(&isend,1,MPI_INT,right,0,MPI_COMM_WORLD);
+	// If there are multiple processes, send information
+	ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag_x,MPI_COMM_WORLD);
+	ierr = MPI_Send(&isend,1,MPI_INT,right,tag_i,MPI_COMM_WORLD);
       }
     }
 
+    // Case for 'middle' processes
     if ((rank>0) && (rank<size-1)){
-	ierr = MPI_Recv(&local_x1,1,MPI_DOUBLE,left,tag,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	ierr = MPI_Recv(&istart,1,MPI_INT,left,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-	local_x1 += dx;
+      // Recieve from left-hand side
+	ierr = MPI_Recv(&local_x1,1,MPI_DOUBLE,left,tag_x,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	ierr = MPI_Recv(&istart,1,MPI_INT,left,tag_i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 	local_x2 = local_x1+(local_npnts*dx);
+	// Calculate starting global index of rightward process
 	isend = istart + local_npnts;
-	ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag,MPI_COMM_WORLD);
-	ierr = MPI_Send(&isend,1,MPI_INT,right,0,MPI_COMM_WORLD);
+	// Send information
+	ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag_x,MPI_COMM_WORLD);
+	ierr = MPI_Send(&isend,1,MPI_INT,right,tag_i,MPI_COMM_WORLD);
       }
 
+    // Special right-most edge case
     if ((rank==size-1) && (rank!=0)){
-      ierr = MPI_Recv(&local_x1,1,MPI_DOUBLE,left,tag,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-      ierr = MPI_Recv(&istart,1,MPI_INT,left,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-      local_x1 += dx;
+      // Recieve from left-hand side
+      ierr = MPI_Recv(&local_x1,1,MPI_DOUBLE,left,tag_x,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+      ierr = MPI_Recv(&istart,1,MPI_INT,left,tag_i,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
       local_x2 = x2;
     }
 
+    // Add guard cells
     local_npnts+=2;
+    // Calculate local_ngrid
     int local_ngrid = (local_x2-local_x1)/dx;
     
-    // Define and allocate arrays.
+    // Define and allocate local arrays.
     rarray<float,1> rho_prev(local_npnts); // time step t-1
     rarray<float,1> rho(local_npnts);      // time step t
     rarray<float,1> rho_next(local_npnts); // time step t+1
@@ -122,7 +144,8 @@ int main(int argc, char* argv[])
 
     // Initialize. Start in the right place and Don't fill guard cells
     for (int i = 1; i < (local_npnts-1); i++) {
-      // i-1 -> i-2 to make sure what would be x[0] -> x[1] to preserve the guard cell
+      // i-1 -> i-2 to make sure what would be x[0] -> x[1] to preserve the guard cells
+      // use the global index to calculate values
       int global_i = istart+i;
       x[i] = x1 + ((global_i-2)*(x2-x1))/ngrid;
       rho[i] = 0.0;
@@ -130,6 +153,7 @@ int main(int argc, char* argv[])
       rho_next[i] = 0.0;
       // if global index in appropriate global range, excite
       if (global_i>(npnts/4)+1 && (global_i<3*npnts/4)){
+	// i -> i-1 to make sure what would be rho[0] -> rho[1] to preserve the guard cells
 	rho[i] = 0.25 - fabs(float((global_i-1)-npnts/2)/float(npnts));
 	rho_prev[i] = rho[i];
 	rho_init[i] = rho[i];
@@ -137,6 +161,9 @@ int main(int argc, char* argv[])
 			     
       }
     }
+    
+    // THE FOLLOWING IS OUTPUT CODE, COMMENTED OUT FOR THE SAKE OF SCALING TESTS
+    
     // Plot or Write out data.
     //std::ofstream dataFile;
     int red, grey, white;
@@ -173,6 +200,7 @@ int main(int argc, char* argv[])
     for (int s = 0; s < nsteps; s++) {
 
         // Set zero dirichlet boundary conditions
+      // Create tags for two send-receive processes
       int tag_to_start = 2;
       int tag_to_end = 3;
       
@@ -180,27 +208,32 @@ int main(int argc, char* argv[])
       // Send appropriate boundary conditions
 	if ((rank>=0) && (rank < size-1)){
 	  float end = rho[local_ngrid-2];
+	  // send end point (value for righthand neighbour's left guard cell)  to the right
 	  ierr = MPI_Send(&end,1,MPI_FLOAT,right,tag_to_start,MPI_COMM_WORLD);
 	}
 	
 	if ((rank > 0) && (rank <= size-1)){
 	  float start = rho[1];
+	  // send start point (value for lefthand neighbour's right guard cell) to the left
 	  ierr = MPI_Send(&start,1,MPI_FLOAT,left,tag_to_end,MPI_COMM_WORLD);
 	}
       // Recieve appropriate boundary conditions
 
 	if ((rank >= 0) && (rank < size-1)){
 	  float end;
+	  // recieve end point (right guard cell) from the right
 	  ierr = MPI_Recv(&end,1,MPI_FLOAT,right,tag_to_end,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 	  rho[local_ngrid-1] = end;
 	}
 	
 	if ((rank > 0) && (rank <= size-1)){
 	  float start;
+	  // recieve start point (left guard cell) from the left
 	  ierr = MPI_Recv(&start,1,MPI_FLOAT,left,tag_to_start,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 	  rho[0] = start;
 	}
 
+	// Apply Dirichlet
 	if (rank==0){
 	  rho[0] = 0.0;
 	}
@@ -209,6 +242,7 @@ int main(int argc, char* argv[])
 	  rho[local_ngrid-1] = 0.0;
 	}
       }
+      // Handle case with only one process
       else if (size == 1){
 	rho[0] = 0.0;
 	rho[local_ngrid-1] = 0.0;
@@ -227,6 +261,8 @@ int main(int argc, char* argv[])
         rho_prev = rho;
         rho      = rho_next;
         rho_next = temp;  
+	
+	//THE FOLLOWING IS OUTPUT CODE, COMMENTED OUT FOR THE SAKE OF SCALING TESTS 
 	
         //Output every nper
         //if ((s+1)%nper == 0) {
@@ -255,9 +291,13 @@ int main(int argc, char* argv[])
     // Output measured runtime.
     std::cout << "Walltime = " << tt.silent_tock() << " sec."  << std::endl;
 
+
+    // THE FOLLOWING IS OUTPUT CODE, COMMENTED OUT FOR THE SAKE OF SCALING TESTS 
     // Close file.
     //if (not graphics)
     //   dataFile.close();
+    
+    // Finish MPI
     ierr = MPI_Finalize();
     
     return 0;
