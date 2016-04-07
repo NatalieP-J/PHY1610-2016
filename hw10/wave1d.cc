48e8f0ef3f1662a446481998a8d16c822e367db9
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2016-04-07 13:02:12 -0400

    Updated code to accept proper Dirichlet conditions, filling guard cells from neighbouring processes:

--- hw10/wave1d.cc
+++ hw10/wave1d.cc
@@ -19,13 +19,6 @@ int main(int argc, char* argv[])
     // Open inifile and parse (using Inifile class from inifile.h)
     Inifile parameter(argc==1?"default.txt":argv[1]);
 
-    // MPI testing stuff add
-    int rank, size;
-    int ierr;
-    ierr = MPI_Init(&argc, &argv);
-    ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    ierr = MPI_Comm_size(MPI_COMM_WORLD,&size);
-
     // Physical parameters
     double  c       = parameter.get<double>("c", 1.0);     // wave speed
     double  tau     = parameter.get<double>("tau", 20.0);  // damping time
@@ -65,12 +58,15 @@ int main(int argc, char* argv[])
     std::cout << "#nper     " << nper    << std::endl;
     std::cout << "#graphics " << int(graphics) << std::endl;
 
-    int local_ngrid = (ngrid/size);
+    int rank, size;
+    int ierr;
+    ierr = MPI_Init(&argc, &argv);
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    ierr = MPI_Comm_size(MPI_COMM_WORLD,&size);
+
     int local_npnts = (npnts/size); // calculate the number of points (plus guard cells)
 
     int left_over_npnts = npnts % size;
-    
-    std::cout << local_npnts << " " << left_over_npnts << std::endl;
 
     if (left_over_npnts != 0){
       for(int assigned_pts = 0; assigned_pts < left_over_npnts; assigned_pts++){
@@ -91,8 +87,10 @@ int main(int argc, char* argv[])
       local_x1 = x1;
       local_x2 = x1+(local_npnts*dx);
       isend += local_npnts;
-      ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag,MPI_COMM_WORLD);
-      ierr = MPI_Send(&isend,1,MPI_INT,right,0,MPI_COMM_WORLD);
+      if (size > 1){
+	ierr = MPI_Send(&local_x2,1,MPI_DOUBLE,right,tag,MPI_COMM_WORLD);
+	ierr = MPI_Send(&isend,1,MPI_INT,right,0,MPI_COMM_WORLD);
+      }
     }
 
     if ((rank>0) && (rank<size-1)){
@@ -105,7 +103,7 @@ int main(int argc, char* argv[])
 	ierr = MPI_Send(&isend,1,MPI_INT,right,0,MPI_COMM_WORLD);
       }
 
-    if (rank==size-1){
+    if ((rank==size-1) && (rank!=0)){
       ierr = MPI_Recv(&local_x1,1,MPI_DOUBLE,left,tag,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
       istart = MPI_Recv(&istart,1,MPI_INT,left,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
       local_x1 += dx;
@@ -113,7 +111,7 @@ int main(int argc, char* argv[])
     }
 
     local_npnts+=2;
-    std::cout << rank << " " << local_x1 << " " << local_x2 << " " << local_npnts << "\n";
+    int local_ngrid = (local_x2-local_x1)/dx;
     
     // Define and allocate arrays.
     rarray<float,1> rho_prev(local_npnts); // time step t-1
@@ -122,16 +120,15 @@ int main(int argc, char* argv[])
     rarray<float,1> rho_init(local_npnts); // initial values
     rarray<float,1> x(local_npnts);        // x values
 
-    int global_i;
     // Initialize. Start in the right place and Don't fill guard cells
     for (int i = 1; i < (local_npnts-1); i++) {
       // i-1 -> i-2 to make sure what would be x[0] -> x[1] to preserve the guard cell
-      global_i = istart+i
+      int global_i = istart+i;
       x[i] = x1 + ((global_i-2)*(x2-x1))/ngrid;
       rho[i] = 0.0;
       rho_prev[i] = 0.0;
       rho_next[i] = 0.0;
-      // if local index in appropriate global range, excite
+      // if global index in appropriate global range, excite
       if (global_i>(npnts/4)+1 && (global_i<3*npnts/4)){
 	rho[i] = 0.25 - fabs(float((global_i-1)-npnts/2)/float(npnts));
 	rho_prev[i] = rho[i];
@@ -140,34 +137,33 @@ int main(int argc, char* argv[])
 			     
       }
     }
-       
     // Plot or Write out data.
-    std::ofstream dataFile;
+    //std::ofstream dataFile;
     int red, grey, white;
 
-    if (graphics) {
-       cpgbeg(0, "/xwindow", 1, 1);
-       cpgask(0);
-       red = 2; cpgscr(red,1.,0.,0.);
-       grey = 3; cpgscr(grey,.2,.2,.2);
-       white = 4; cpgscr(white,1.0,1.0,1.0);
-       cpgsls(1); cpgslw(6); cpgsci(white);
-       cpgslw(2);
-       cpgenv(x1, x2, 0., 0.25, 0, 0);
-       cpglab("x", "rho", "Wave Test");
-       cpgsls(1); cpgslw(6); cpgsci(white);
-       cpgline(npnts, x.data(), rho.data());
-       cpgsls(2); cpgslw(12); cpgsci(red);
-       cpgline(npnts, x.data(), &rho_init[0]);
-    } else {     
-       dataFile.open(dataFilename.c_str());
-       dataFile << nper << ","   
-                << npnts       << "\n";
-       dataFile << time << "\n";
-       for (int i = 0; i < npnts; i++ ) 
-          dataFile << x[i] << " " << rho[i] << " \n";  
-       dataFile << "\n";
-    }
+    //if (graphics) {
+    //   cpgbeg(0, "/xwindow", 1, 1);
+    //   cpgask(0);
+    //   red = 2; cpgscr(red,1.,0.,0.);
+    //   grey = 3; cpgscr(grey,.2,.2,.2);
+    //   white = 4; cpgscr(white,1.0,1.0,1.0);
+    //   cpgsls(1); cpgslw(6); cpgsci(white);
+    //   cpgslw(2);
+    //   cpgenv(x1, x2, 0., 0.25, 0, 0);
+    //   cpglab("x", "rho", "Wave Test");
+    //   cpgsls(1); cpgslw(6); cpgsci(white);
+    //   cpgline(npnts, x.data(), rho.data());
+    //   cpgsls(2); cpgslw(12); cpgsci(red);
+    //   cpgline(npnts, x.data(), &rho_init[0]);
+    //} else {     
+    //   dataFile.open(dataFilename.c_str());
+    //   dataFile << nper << ","   
+    //            << npnts       << "\n";
+    //   dataFile << time << "\n";
+    //   for (int i = 0; i < npnts; i++ ) 
+    //      dataFile << x[i] << " " << rho[i] << " \n";  
+    //   dataFile << "\n";
+    //}
      
     // measure time
     TickTock tt;
@@ -177,11 +173,49 @@ int main(int argc, char* argv[])
     for (int s = 0; s < nsteps; s++) {
 
         // Set zero dirichlet boundary conditions
-        rho[0] = 0.0;
-        rho[ngrid+1] = 0.0;
+      int tag_to_start = 2;
+      int tag_to_end = 3;
+      
+      if (size > 1){
+      // Send appropriate boundary conditions
+	if ((rank>=0) && (rank < size-1)){
+	  float end = rho[local_ngrid-2];
+	  ierr = MPI_Send(&end,1,MPI_FLOAT,right,tag_to_start,MPI_COMM_WORLD);
+	}
+	
+	if ((rank > 0) && (rank <= size-1)){
+	  float start = rho[1];
+	  ierr = MPI_Send(&start,1,MPI_FLOAT,left,tag_to_end,MPI_COMM_WORLD);
+	}
+      // Recieve appropriate boundary conditions
 
-        // Evolve
-        for (int i = 1; i <= ngrid; i++) {
+	if ((rank >= 0) && (rank < size-1)){
+	  float end;
+	  ierr = MPI_Recv(&end,1,MPI_FLOAT,right,tag_to_end,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  rho[local_ngrid-1] = end;
+	}
+	
+	if ((rank > 0) && (rank <= size-1)){
+	  float start;
+	  ierr = MPI_Recv(&start,1,MPI_FLOAT,left,tag_to_start,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+	  rho[0] = start;
+	}
+
+	if (rank==0){
+	  rho[0] = 0.0;
+	}
+	
+	if (rank==(size-1)){
+	  rho[local_ngrid-1] = 0.0;
+	}
+      }
+      else if (size == 1){
+	rho[0] = 0.0;
+	rho[local_ngrid-1] = 0.0;
+      }
+      
+      // Evolve
+        for (int i = 1; i <= local_ngrid; i++) {
             float laplacian = pow(c/dx,2)*(rho[i+1] + rho[i-1] - 2*rho[i]);
             float friction = (rho[i] - rho_prev[i])/tau;
             rho_next[i] = 2*rho[i] - rho_prev[i] + dt*(laplacian*dt-friction);
@@ -195,35 +229,35 @@ int main(int argc, char* argv[])
         rho_next = temp;  
 	
         //Output every nper
-        if ((s+1)%nper == 0) {
-           if (graphics) {
-              cpgbbuf();
-              cpgeras();
-              cpgsls(1); cpgslw(6); cpgsci(white);
-              cpgslw(2);
-              cpgenv(x1, x2, 0., 0.25, 0, 0);
-              cpglab("x", "rho", "Wave test");  //t=s*dt
-              cpgsls(2); cpgslw(12); cpgsci(red);
-              cpgline(npnts, x.data(), rho.data());
-              cpgsls(1); cpgslw(6); cpgsci(white);
-              cpgline(npnts, x.data(), rho_init.data());
-              cpgebuf();
-              sleep(1); // artificial delay! 
-           } else {
-              dataFile << time << "\n";
-              for (int i = 0; i < npnts; i++ ) 
-                 dataFile<< x[i] << " " << rho[i] << "\n"; 
-              dataFile << "\n";
-           } 
-        }
+        //if ((s+1)%nper == 0) {
+        //   if (graphics) {
+        //      cpgbbuf();
+        //      cpgeras();
+        //      cpgsls(1); cpgslw(6); cpgsci(white);
+        //      cpgslw(2);
+        //      cpgenv(x1, x2, 0., 0.25, 0, 0);
+        //      cpglab("x", "rho", "Wave test");  //t=s*dt
+        //      cpgsls(2); cpgslw(12); cpgsci(red);
+        //      cpgline(npnts, x.data(), rho.data());
+        //      cpgsls(1); cpgslw(6); cpgsci(white);
+        //      cpgline(npnts, x.data(), rho_init.data());
+        //      cpgebuf();
+        //      sleep(1); // artificial delay! 
+        //   } else {
+        //      dataFile << time << "\n";
+        //      for (int i = 0; i < npnts; i++ ) 
+        //         dataFile<< x[i] << " " << rho[i] << "\n"; 
+        //      dataFile << "\n";
+        //   } 
+        //}
     }
     
     // Output measured runtime.
     std::cout << "Walltime = " << tt.silent_tock() << " sec."  << std::endl;
 
     // Close file.
-    if (not graphics)
-       dataFile.close();
+    //if (not graphics)
+    //   dataFile.close();
     ierr = MPI_Finalize();
     
     return 0;
